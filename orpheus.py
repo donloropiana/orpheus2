eea53b93e25048a055ca2e2f0755c044e7c8ff6e
mbb525@nyu.edu <maxbushala@Maxs-MBP-4.lan>
2024-04-26 15:53:23 -0400

    Commit 1 apr 26 2024

--- orpheus.py
+++ orpheus.py
@@ -758,8 +758,8 @@ class valuation:
                     projection_table['future_years'][f'Year {year+1}']['Tax Rate'] = projection_table['base_year']['Tax Rate']
 
                     # setting the year 1 NOPAT line item
-                    if(projection_table['future_years'][f'Year {year+1}']['Operating Income'] > 0): # .iloc[0]
-                        if(projection_table['future_years'][f'Year {year+1}']['Operating Income'] < projection_table['base_year']['NOL']): #.iloc[0]
+                    if(projection_table['future_years'][f'Year {year+1}']['Operating Income'].item() > 0): # .iloc[0]
+                        if(projection_table['future_years'][f'Year {year+1}']['Operating Income'].item() < projection_table['base_year']['NOL']): #
                             projection_table['future_years'][f'Year {year+1}']['NOPAT'] = projection_table['future_years'][f'Year {year+1}']['Operating Income']
                         else:
                             projection_table['future_years'][f'Year {year+1}']['NOPAT'] = projection_table['future_years'][f'Year {year+1}']['Operating Income'] - (projection_table['future_years'][f'Year {year+1}']['Operating Income'] - projection_table['base_year']['NOL']) * projection_table['future_years'][f'Year {year+1}']['Tax Rate']
