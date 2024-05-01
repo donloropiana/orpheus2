02b09efb19ecb98981b985a9437f501d842ca0d5
Noah Perelmuter <np2446@stern.nyu.edu>
2024-05-01 17:00:06 -0400

    uploaded stock data to  sql, added function to retrieve it to sql.py (renamed auth.py)

--- modules/sql.py
+++ modules/sql.py
@@ -2,10 +2,11 @@ import mysql.connector
 import bcrypt
 from contextlib import contextmanager
 from dotenv import dotenv_values
+import pandas as pd
 
 # Load environment variables
 config = dotenv_values(".env")
-password = config['DB_PASSWORD']
+password = 'N3G+LzHFk9c='
 
 # Database connection details
 connection_config = {
@@ -60,3 +61,14 @@ def verify_password(username: str, password: str) -> bool:
             stored_password = result[0]
             return bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8'))
         return False
+
+# Function to convert a table to a pandas DataFrame
+# example: table_to_df('stock_data')
+def table_to_df(table_name: str) -> pd.DataFrame:
+    query = f"SELECT * FROM {table_name}"
+    with get_db_connection() as conn, get_cursor(conn) as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        return pd.DataFrame(rows, columns=columns)
+    
